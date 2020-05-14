ab52ba04f410c606c53a76e605951122cb9da01e
SomeAsianDude <63079694+SomeAsianDude@users.noreply.github.com>
2020-05-14 19:59:36 -0400

    Add files via upload

--- Sabotage.js
+++ Sabotage.js
@@ -1,29 +1,54 @@
-console.log("dead");
-const form = document.querySelector(".form");
+// console.log("dead");
+// const form = document.getElementsByClassName("form");
+// console.log(form);
 
-form.addEventListener("submit", getcreditinfo);
+// form.addEventListener("submit", getcreditinfo);
 
-const checkbox = document.querySelector("#agree");
-const button = document.querySelector("#submitButton");
+// const checkbox = document.querySelector("#agree");
+// const button = document.querySelector("#submitButton");
 
-function getcreditinfo(e) {
-  e.preventDefault();
-  if (document.getElementById("agree").checked == false) {
-    alert("By signing up, you must accept our terms and conditions!");
-    return false;
+// function getcreditinfo(e) {
+//   e.preventDefault();
+//   if (document.getElementById("agree").checked == false) {
+//     alert("By signing up, you must accept our terms and conditions!");
+//     return false;
+//   }
+
+//   const number = document.querySelector("#number");
+//   if (number.value.length != 16) {
+//     alert("Your credit card number must be exactly 16 digits");
+//     return false;
+//   }
+//   const date = document.querySelector("#date");
+//   const number1 = document.querySelector("#number1");
+//   const words = document.querySelector("#words");
+
+//   console.log(number.value);
+//   console.log(date.value);
+//   console.log(number1.value);
+//   console.log(words.value);
+// }
+
+var imgIndex = 1;
+showImages(imgIndex);
+
+function changeImage(n) {
+  showImages((imgIndex += n));
+}
+
+function showImages(n) {
+  var images = document.getElementsByClassName("mySlide");
+  if (n > images.length) {
+    imgIndex = 1;
   }
 
-  const number = document.querySelector("#number");
-  if (number.value.length != 16) {
-    alert("Your credit card number must be exactly 16 digits");
-    return false;
+  if (n < 1) {
+    imgIndex = images.length;
   }
-  const date = document.querySelector("#date");
-  const number1 = document.querySelector("#number1");
-  const words = document.querySelector("#words");
 
-  console.log(number.value);
-  console.log(date.value);
-  console.log(number1.value);
-  console.log(words.value);
+  for (let i = 0; i < images.length; i++) {
+    images[i].style.display = "none";
+  }
+
+  images[imgIndex - 1].style.display = "block";
 }
